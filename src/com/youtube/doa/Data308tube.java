ebb90ec0a0e765b25a1c94f8c0e9d017843ca6eb
dbrooksss <daniellebrooks07@yahoo.com>
2016-12-21 14:46:16 -0500

    changes 12.21.16

---
+++ src/com/youtube/doa/Data308tube.java
@@ -0,0 +1,21 @@
+package com.youtube.doa;
+
+import javax.naming.*;
+import javax.sql.*;
+
+public class Data308tube{
+	
+	private static DataSource Data308tube = null;
+	private static Context context = null;
+	
+	public static DataSource Data308tubeConn(){
+		return Data308tube;
+		
+	}
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+
+	}
+
+}
