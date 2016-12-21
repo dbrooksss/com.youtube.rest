4d85b6557d2603265628138fcbe87b25e1f035fa
dbrooksss <daniellebrooks07@yahoo.com>
2016-12-21 16:48:06 -0500

    more

--- src/com/youtube/doa/Data308tube.java
+++ src/com/youtube/doa/Data308tube.java
@@ -4,13 +4,30 @@ import javax.naming.*;
 import javax.sql.*;
 
 public class Data308tube{
-	
+
 	private static DataSource Data308tube = null;
 	private static Context context = null;
-	
-	public static DataSource Data308tubeConn(){
+
+	public static DataSource Data308tubeConn() throws Exception{
+		// 2 private variables to prevent tampering
+		// methods are going to call this method to get db connection for data
+
+		if(Data308tube != null){
+			return Data308tube;
+		}
+
+		try{
+			if(context == null){
+				context = new InitialContext();
+			}
+
+			// looking up from weblogic setup
+			Data308tube = (DataSource) context.lookup("308tubeData");
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return Data308tube;
-		
 	}
 
 	public static void main(String[] args){
--- src/com/youtube/rest/status/V1_status.java
+++ src/com/youtube/rest/status/V1_status.java
@@ -1,7 +1,9 @@
 package com.youtube.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+import java.sql.*;
+import com.youtube.doa.*;
 
 @Path("/v1/status")
 public class V1_status{
@@ -19,6 +21,41 @@ public class V1_status{
 	@Produces(MediaType.TEXT_HTML)
 	public String returnVersion(){
 		return "<p>Version: </p>" + apiVersion;
+	}
+
+	@Path("/database") // if has path of db, run method
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception{
+		PreparedStatement query = null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+//		// 1. get db connection
+//		// 2. write sql query & have java compile it
+//		// 3. send pre-compiled to db for data
+//
+//		try{
+//			conn = Data308tube.Data308tubeConn().getConnection();
+//			query = conn.prepareStatement("Select to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS') DATETIME from sys.dual");
+//			ResultSet rs = query.executeQuery();
+//
+//			while(rs.next()){ // loop through rs
+//				// debug println(rs.getString("DATETIME"));
+//				myString = rs.getString("DATETIME");
+//			}
+//			query.close(); // close connection ***
+//			returnString = "<p>Database Status</p> " + "<p>Database Date/Time return: " + myString + "</p>";
+//
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}finally{ // should have if db connection
+//			if(conn != null){
+//				conn.close();
+//			}
+//		}
+//		return returnString;
+		return "<p>Have to work on this db method! </p>";
 	}
 }
 
